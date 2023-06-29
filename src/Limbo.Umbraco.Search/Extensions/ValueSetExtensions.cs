54af25b1169398dd52374486f533a5b070b46c1e
Anders Bjerner <abjerner@skybrud.dk>
2023-06-29 10:24:18 +0200

    A little code cleanup

--- src/Limbo.Umbraco.Search/Extensions/ValueSetExtensions.cs
+++ src/Limbo.Umbraco.Search/Extensions/ValueSetExtensions.cs
@@ -76,11 +76,11 @@ namespace Limbo.Umbraco.Search.Extensions {
                 throw new Exception($"'{nameof(valueSet.Values)}' is not an instance of '{typeof(IDictionary<string, IReadOnlyList<object>>)}'");
             }
 
-            if (value is IReadOnlyList<object> list) {
-                dictionary.Add(key, list);
-            } else {
-                dictionary.Add(key, new List<object> { value });
-            }
+            // Convert "value" to a list if it's not already a list
+            if (value is not IReadOnlyList<object> list) list = new List<object> { value };
+
+            // Add the list to the dictionary
+            dictionary.Add(key, list);
 
             return true;
 
