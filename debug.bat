1045d38ae90bba0515e3864d0472cd05e8ce9efb
Anders Bjerner <abjerner@skybrud.dk>
2024-02-27 14:12:46 +0100

    First alpha release for Umbraco 13 :tada:

--- debug.bat
+++ debug.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Limbo.Umbraco.Search --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco10
+dotnet build src/Limbo.Umbraco.Search --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco13
